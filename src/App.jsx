b808d9b9b6ca9738236b1ca9d1cbb195ff9f9e72
Chethan <chethan962089@gmail.com>
2025-02-22 16:40:05 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,16 @@
 import React from 'react';
 import {  Route, Routes, Navigate } from 'react-router-dom';
 
-
-
-// const ProtectedRoute = ({ children, roles }) => {
-//     const { isAuthenticated, } = useAuth();
-//      const  user ='admin'
-//     console.log('user', user );
-   
-//     if (!isAuthenticated) {
-//         console.warn('User is not authenticated. Redirecting to login.');
-//         return <Navigate to="/login" />;
-//     }
-
-//     if (roles && (!user || !roles.includes(user))) {
-//         console.log('User does not have the required role. Redirecting to dashboard.');
-//         return <Navigate to="/dashboard" />;
-//     }
-
-//     return children;
-// };
-
-
-
-
-const hancleclick= async()=>{
- const responce = await  axiosClient.get("create")
-
- console.log(responce.data);
- 
-}
-
 const App = () => {
 
-    // const { isAuthenticated, user } = useAuth();
+   
     return (
       
             <Routes>
-                <Route path="/" element={<h1> THis is the landing page </h1>} />
-                
+                <Route path="/" element={<h1> THis is the landing page hiii  man ss </h1>} />
+            
 
-
-             {/*  This is Parent route  */}
+               {/*  This is Parent route  */}
                 <Route path="/dashboard" element={ <h1> this is the dash board </h1>}> 
                 {/*  this is chiled routes  */}
                  <Route index element={<h1> its a dashbord content
@@ -70,26 +39,3 @@ const NotFound =()=>{
     <h1 className=' items-center test-2xl'> Page Not Found </h1>
     </>
 }
-
-
-{/* <Routes>
-<Route path="/" element={<h1> THis is the landing page </h1>} />
-<Route     path="/login"element={isAuthenticated ? <Navigate to="/dashboard"
- replace /> : <LoginPage />}/> */}
-
-
-
-{/* <Route path="/dashboard" element={ <ProtectedRoute > <Dashboard /></ProtectedRoute>}>  */}
-
-//  <Route index element={<h1> its a dashbord content
-
-//     <br />
-//     <button onClick={hancleclick}> click here  </button>
-//      </h1>} />
-//  <Route path="drivers" element={<ProtectedRoute roles={[ 'user', 'admin' ]}><Drivers/></ProtectedRoute>} />
-//  <Route path="settings" element={<ProtectedRoute roles={['admin']}><h1> this is  Settings </h1></ProtectedRoute>} />
-// </Route>
-
-
-// <Route path="*" element={<NotFound/>} />    
-// </Routes>
